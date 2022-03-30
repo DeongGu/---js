b4495d418ae590910d4b224ffd6a342458067a47
Seong Woo <95382684+DeongGu@users.noreply.github.com>
2022-03-30 18:43:55 +0900

    현재 위치 설정

--- js/background.js
+++ js/background.js
@@ -4,4 +4,4 @@ let images = ["20191020.jpg","20170116.jpg","1585027115011.jpg"];
 
 let chosenImage = images[Math.floor(Math.random() * images.length)];
 
-bgImage.style.backgroundImage = `url('/image/${chosenImage}'`;
+bgImage.style.backgroundImage = `url('./image/${chosenImage}'`;
